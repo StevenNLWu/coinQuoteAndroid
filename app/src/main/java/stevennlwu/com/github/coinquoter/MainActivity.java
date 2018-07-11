e61d96ef162fc7a5ca84d56a6bd58b4b0c981fe3
StevenNLWu <GitHub556Acc@outlook.com>
2018-07-11 14:46:16 +0800

    refer a .xml for dynamic UI generation

--- app/src/main/java/stevennlwu/com/github/coinquoter/MainActivity.java
+++ app/src/main/java/stevennlwu/com/github/coinquoter/MainActivity.java
@@ -36,61 +36,18 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.navigation_all:
 
                     /* Find Tablelayout defined in main.xml */
-                    TableLayout mainTable = (TableLayout) findViewById(R.id.mainTable01);
+                    TableLayout mainTable = findViewById(R.id.mainTable01);
 
-                    /* Create a new row to be added. */
-                    TableRow newTRow = new TableRow(MainActivity.this);
-                    newTRow.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT
-                                                                    , TableRow.LayoutParams.MATCH_PARENT));
-                    newTRow.requestLayout();
+                    /* refer the TableRow from .xml */
+                    TableRow row = findViewById(R.id.tr);
 
-                    /* Create Grid */
-                    GridLayout newGrid = new GridLayout(MainActivity.this);
-                    newGrid.setRowCount(2);
-                    newGrid.setColumnCount(4);
-                    newGrid.requestLayout();
-
-                    /* Create image */
-                    ImageView image = new ImageView(MainActivity.this);
-                    image.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT
-                                                                    , ViewGroup.LayoutParams.MATCH_PARENT));
-                    image.setImageResource(R.drawable.coinmarket);
-                    image.setBaselineAlignBottom(true);
-                    GridLayout.LayoutParams GridParams = new GridLayout.LayoutParams();
-                    GridParams.setMargins(6,6,6,6);
-                    GridParams.rowSpec = GridLayout.spec(0, 2);
-                    GridParams.columnSpec = GridLayout.spec(0, 1);
-                    // Once you are done set the LayoutParams to the layout
-                    image.setLayoutParams(GridParams);
-                    image.requestLayout();
-
-                    /* Create text 1 */
-                    TextView newText1 = new TextView(MainActivity.this);
-                    RelativeLayout.LayoutParams Textparams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT
-                                                                                            , RelativeLayout.LayoutParams.MATCH_PARENT);
-                    Textparams.addRule(RelativeLayout.ALIGN_PARENT_END, 1);
-                    Textparams.addRule(RelativeLayout.ALIGN_PARENT_TOP, 1);
-                    newText1.setGravity(Gravity.CENTER );
-                    Textparams.setMargins(6,6,6,6);
-                    newText1.setText("BTC");
-                    GridParams.rowSpec = GridLayout.spec(0, 2);
-                    GridParams.columnSpec = GridLayout.spec(0, 1);
-                    // Once you are done set the LayoutParams to the layout
-                    newText1.setLayoutParams(GridParams);
-                    newText1.requestLayout();
-                    newText1.setLayoutParams(Textparams);
-                    newText1.requestLayout();
-
-
-
+                    /* refer the  from .xml */
+                    ((TextView)row.findViewById(R.id.assetName)).setText("AST");
 
                     /* add all together */
-                    newGrid.addView(image);
-                    newGrid.addView(newText1);
-                    //newGrid.addView(newText2);
-                    //newGrid.addView(newText3);
-                    newTRow.addView(newGrid);
-                    mainTable.addView(newTRow);
+                    row.requestLayout();
+                    mainTable.addView(row);
+                    mainTable.requestLayout();
 
                     return true;
 
