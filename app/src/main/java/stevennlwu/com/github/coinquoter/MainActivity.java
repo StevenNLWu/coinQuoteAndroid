bf35800b8a47c7bda85ecaec931c74b3bfc2c64e
StevenNLWu <GitHub556Acc@outlook.com>
2018-09-17 17:46:54 +0800

    in case of adding a textView, the dynamic UI works; in case of adding image, it doesn't work. try to figure it out later

--- app/src/main/java/stevennlwu/com/github/coinquoter/MainActivity.java
+++ app/src/main/java/stevennlwu/com/github/coinquoter/MainActivity.java
@@ -1,15 +1,18 @@
 package stevennlwu.com.github.coinquoter;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.TypedValue;
 import android.view.MenuItem;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -18,6 +21,8 @@ import model.Listings;
 import model.Ticker;
 import model.aListings;
 import model.aTicker;
+
+import static java.security.AccessController.getContext;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,26 +35,51 @@ public class MainActivity extends AppCompatActivity {
             switch (item.getItemId()) {
                 case R.id.navigation_all:
 
+
                     /* Find Tablelayout defined in main.xml */
                     TableLayout mainTable = findViewById(R.id.TableLayout);
 
                     /* Create a new row to be added. */
-                    TableRow newTRow = new TableRow(MainActivity.this);
-                    newTRow.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT
-                                                                    , TableRow.LayoutParams.MATCH_PARENT));
-                    newTRow.requestLayout();
+                    //TableRow newTRow = new TableRow(MainActivity.this);
+                    //newTRow.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT
+                    //                                                , TableRow.LayoutParams.MATCH_PARENT));
+                    //newTRow.requestLayout();
 
                     /* Create image */
-                    ImageView image = new ImageView(MainActivity.this);
-                    ViewGroup.MarginLayoutParams marginParams = new ViewGroup.MarginLayoutParams(image.getLayoutParams());
-                    marginParams.setMargins(6, 6, 6, 6);
-                    RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(marginParams);
-                    image.setLayoutParams(layoutParams);
-                    image.requestLayout();
+                    //ImageView image = new ImageView(MainActivity.this);
+                    //ViewGroup.MarginLayoutParams marginParams = new ViewGroup.MarginLayoutParams(image.getLayoutParams());
+                    //image.setPadding(6, 6, 6, 6);
+                    //RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(marginParams);
+                    //image.setLayoutParams(layoutParams);
+                    //image.requestLayout();
 
                     /* add all together */
-                    newTRow.addView(image);
-                    mainTable.addView(newTRow);
+                    //newTRow.addView(image);
+                    //mainTable.addView(newTRow);
+
+
+                    TableRow row = new TableRow(MainActivity.this);
+                    row.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.MATCH_PARENT));
+
+
+                    ImageView image = new ImageView(MainActivity.this);
+                    image.setImageResource(R.drawable.btc);
+                    image.setPadding(6, 6, 6, 6);
+                    image.setId(2);
+
+                    RelativeLayout relativeLayout = new RelativeLayout(MainActivity.this);
+
+
+
+/*
+                    TypedValue outValue = new TypedValue();
+                    MainActivity.this.getTheme().resolveAttribute(android.R.attr.listPreferredItemHeight, outValue, true);
+                    cardView.setBackgroundResource(outValue.resourceId);
+                    */
+
+                    relativeLayout.addView(image);
+                    row.addView(relativeLayout);
+                    mainTable.addView(row);
 
                     return true;
 
