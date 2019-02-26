334e34fa7102016400e7dd9c600207db5d39bd45
StevenNLWu <GitHub556Acc@outlook.com>
2019-02-26 17:39:11 +0800

    sample code to demo how to add image dynamically

--- app/src/main/java/stevennlwu/com/github/coinquoter/MainActivity.java
+++ app/src/main/java/stevennlwu/com/github/coinquoter/MainActivity.java
@@ -1,8 +1,7 @@
 package stevennlwu.com.github.coinquoter;
 
 import android.annotation.SuppressLint;
-import android.app.Activity;
-import android.graphics.Color;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -11,52 +10,61 @@ import android.util.TypedValue;
 import android.view.MenuItem;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
-import android.widget.TextView;
-
 import java.util.ArrayList;
-
 import apiClient.CoinMarketClient;
 import model.Listings;
 import model.Ticker;
 import model.aListings;
 import model.aTicker;
 
+import static android.view.View.inflate;
 import static java.security.AccessController.getContext;
 
 public class MainActivity extends AppCompatActivity {
 
     // event listener for the Top-menu-bar
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener;
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
-    {
-        mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @SuppressLint("ResourceType")
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
-            @SuppressLint("ResourceType")
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()) {
-                    case R.id.navigation_all:
+            // get value of “listPreferredItemHeight” attribute
+            android.util.TypedValue value = new android.util.TypedValue();
+            boolean b = getTheme().resolveAttribute(android.R.attr.listPreferredItemHeight, value, true);
+            String s = TypedValue.coerceToString(value.type, value.data);
+            android.util.DisplayMetrics metrics = new android.util.DisplayMetrics();
+            getWindowManager().getDefaultDisplay().getMetrics(metrics);
+            int preferredHeight = (int) value.getDimension(metrics);
 
-                    /* ind Tablelayout defined in main.xml */
-                    TableLayout mainTable = findViewById(R.id.TableLayout);
+            // get the marginl, from pid to px
+            float marginEnd = getResources().getDimension(R.dimen.image_marginEnd);
+            Resources r = MainActivity.this.getResources();
+            int px = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, marginEnd, r.getDisplayMetrics());
 
-                    // remove all row except the header
-                        /*
-                    int childCount = mainTable.getChildCount();
-                    if (childCount > 1) {
-                        mainTable.removeViews(1, childCount - 1);
-                    }
-                    */
+            switch (item.getItemId()) {
+                case R.id.navigation_all:
 
+                /* ind Tablelayout defined in main.xml */
+                TableLayout mainTable = findViewById(R.id.TableLayout_template);
+
+                // remove all row except the header
+                    /*
+                int childCount = mainTable.getChildCount();
+                if (childCount > 1) {
+                    mainTable.removeViews(1, childCount - 1);
+                }
+
+                */
                     // create a new row
                     TableRow tbRow = new TableRow(MainActivity.this);
 
                     // create a new RelativeLayout
                     RelativeLayout rl =new RelativeLayout(MainActivity.this);
-                    rl.setBackgroundColor(2);
 
                     // create a new Image
                     ImageView image = new ImageView(MainActivity .this);
@@ -64,82 +72,70 @@ public class MainActivity extends AppCompatActivity {
                     image.setId(2);
 
                     // set the image params
-
-                        android.util.TypedValue value = new android.util.TypedValue();
-                        boolean b = getTheme().resolveAttribute(android.R.attr.listPreferredItemHeight, value, true);
-                        String s = TypedValue.coerceToString(value.type, value.data);
-                        android.util.DisplayMetrics metrics = new android.util.DisplayMetrics();
-                        getWindowManager().getDefaultDisplay().getMetrics(metrics);
-                        float ret = value.getDimension(metrics);
-
-                        TypedValue value = new TypedValue();
-                        ((Activity)context).getTheme().resolveAttribute(android.R.attr.listPreferredItemHeight, value, true);
-
-                    RelativeLayout.LayoutParams imageParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT,  h);
-                    imageParams.setMarginEnd((int) getResources().getDimension(R.dimen.image_marginEnd));
-                    image.setLayoutParams(imageParams);
+                    RelativeLayout.LayoutParams imageParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.MATCH_PARENT);
+                    imageParams.setMargins(px,px,px,px);
 
                     // add view component by component
-                    rl.addView(image,imageParams);
+                    mainTable.addView(tbRow);
                     tbRow.addView(rl);
-                    mainTable.addView(tbRow);
+                    rl.addView(image, imageParams);
+
+                    // set the RelativeLayout params
+                    rl.getLayoutParams().width= RelativeLayout.LayoutParams.MATCH_PARENT;
+                    rl.getLayoutParams().height= preferredHeight;
+
+                    // set the TableRow params
+                    tbRow.getLayoutParams().width= RelativeLayout.LayoutParams.MATCH_PARENT;
+                    tbRow.getLayoutParams().height= RelativeLayout.LayoutParams.MATCH_PARENT;
+
+                    /* refer to a template View to set the layout params
+
+                                    // create a new row
+                                    TableRow tbRow = new TableRow(MainActivity.this);
+
+                                    // create a new RelativeLayout
+                                    RelativeLayout rl =new RelativeLayout(MainActivity.this);
+                                    rl.setBackgroundColor(2);
+
+                                    // set the RelativeLayout params
+                                    RelativeLayout rl_template = findViewById(R.id.RelativeLayout_template);
+                                    ViewGroup.LayoutParams rlParams = rl_template.getLayoutParams();
+                                    rl.setLayoutParams(rlParams);
+
+                                    // create a new Image
+                                    ImageView image = new ImageView(MainActivity .this);
+                                    image.setImageResource(R.drawable.btc);
+                                    image.setId(2);
+
+                                    // set the image params
+                                    ImageView img_template = findViewById(R.id.imageView_template);
+                                    ViewGroup.LayoutParams imgParams = img_template.getLayoutParams();
+                                    image.setLayoutParams(imgParams);
+
+                                    // add view component by component
+                                    rl.addView(image);
+                                    tbRow.addView(rl);
+                                    mainTable.addView(tbRow);
+
+                                    /*
 
 
-                    /*
-                    RelativeLayout.LayoutParams firstImageParams =  new RelativeLayout.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT);
-
-                    /*
-                    int pixelsValue = 6; // margin in pixels
-                    float d = MainActivity.this.getResources().getDisplayMetrics().density;
-                    int margin = (int)(pixelsValue * d);
-                    firstImageParams.setMarginEnd(margin);
-
-                    tbRow.addView(rl);
-                    mainTable.addView(tbRow);
-                    */
-
-
-                    // test add image
-                        /*
-                    RelativeLayout channel=new RelativeLayout(MainActivity.this);
-                    channel.setBackgroundColor(2);
-                    ImageView imageTest = new ImageView(MainActivity .this);
-                    imageTest.setImageResource(R.drawable.btc);
-                    imageTest.setId(2);
-                    RelativeLayout.LayoutParams firstImageParams = new RelativeLayout.LayoutParams(
-                            android.view.ViewGroup.LayoutParams.WRAP_CONTENT,
-                            android.view.ViewGroup.LayoutParams.WRAP_CONTENT);
-                    firstImageParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
-                    channel.addView(imageTest,firstImageParams);
-                    tbRow.addView(channel);
-                    mainTable.addView(tbRow);
-                    */
+                                     */
 
 
 
-                        // test can i do dynamical display
-                        /*
-                        TextView tv0 = new TextView(MainActivity.this);
-                        tv0.setText(" Sl.No ");
-                        tv0.setTextColor(Color.BLUE);
-                        tbrow0.addView(tv0);
+                return true;
 
-                        mainTable.addView(tbrow0);
-                        */
+                case R.id.navigation_save:
 
                     return true;
+                case R.id.navigation_setting:
 
-                    case R.id.navigation_save:
-
-                        return true;
-                    case R.id.navigation_setting:
-
-                        return true;
-                }
-                return false;
+                    return true;
             }
-        };
-    }
+            return false;
+        }
+    };
 
 
     private CoinMarketClient aClient;
@@ -148,6 +144,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         // declare menu
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
